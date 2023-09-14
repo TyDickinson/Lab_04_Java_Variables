130a0cdbba75ae6a8150e505971c6c6881f14ab4
TyDickinson <dickintb@mail.uc.edu>
2023-09-14 13:19:29 -0400

    first

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // intOperandA, intOperandB, intSum, intDifference, intQuotient, intModulo
+        int intOperandA = 1;
+        int intOperandB = 6;
+
+
+        int intSum = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        // rest of the int statements go here
+
+        double doubleOperandA = 25.1;
+        double doubleOperandB = 5.6;
+        double doubleSum = doubleOperandA + doubleOperandB
+
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+    }
+}
